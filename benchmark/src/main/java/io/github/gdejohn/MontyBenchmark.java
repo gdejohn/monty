3605ff9e26f02161cf1471b902575ab2c49650c5
Griffin DeJohn <gdejohn@gmail.com>
2023-10-22 11:23:46 -0600

    Create Maven multi module project with benchmark

---
+++ benchmark/src/main/java/io/github/gdejohn/MontyBenchmark.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.github.gdejohn;
+
+import io.github.gdejohn.monty.Hand;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+
+import static io.github.gdejohn.monty.Card.Rank.ACE;
+import static io.github.gdejohn.monty.Card.Rank.EIGHT;
+import static io.github.gdejohn.monty.Card.Rank.NINE;
+import static io.github.gdejohn.monty.Card.Rank.SEVEN;
+import static io.github.gdejohn.monty.Card.Rank.TEN;
+import static io.github.gdejohn.monty.Card.Suit.CLUBS;
+import static io.github.gdejohn.monty.Card.Suit.DIAMONDS;
+import static io.github.gdejohn.monty.Card.Suit.HEARTS;
+import static io.github.gdejohn.monty.Card.Suit.SPADES;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.openjdk.jmh.annotations.Mode.Throughput;
+
+public class MontyBenchmark {
+    @Benchmark
+    @BenchmarkMode(Throughput)
+    @OutputTimeUnit(SECONDS)
+    public int evaluate() {
+        var hand = Hand.empty();
+        hand.deal(SEVEN.of(CLUBS));
+        hand.deal(TEN.of(CLUBS));
+        hand.deal(ACE.of(HEARTS));
+        hand.deal(ACE.of(SPADES));
+        hand.deal(ACE.of(DIAMONDS));
+        hand.deal(EIGHT.of(CLUBS));
+        hand.deal(NINE.of(CLUBS));
+        return hand.evaluate();
+    }
+}
